75808dd4e7262944cb6df0dfc27c52da2e18295d
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-05-14 06:49:07 -0300

    mejora en navBar y agrego search

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ export function Header() {
   return (
     <header>
       <div className="container">
-        <nav className="navbar navbar-light bg-light">
-          <div className="container-fluid">
+        {/* <nav className="navbar navbar-light bg-primary">
+          <div className="container">
             <Link to="/" className="navbar-brand mb-0 h1">
               Home
             </Link>
@@ -13,18 +13,34 @@ export function Header() {
             <Link to="/listado" className="navbar-brand mb-0 h1">
               Listado
             </Link>
+          </div>
+        </nav> */}
 
-            <Link to="/contacto" className="navbar-brand mb-0 h1">
-              Contacto
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link to={"/"} className="navbar-brand mb-0 h1" href="a">
+              Login
             </Link>
-
-            <Link to="/testing" className="navbar-brand mb-0 h1">
-              test
+            <Link to="/listado" className="navbar-brand mb-0 h1">
+              Listado
             </Link>
-
-            <Link to="/testing2" className="navbar-brand mb-0 h1">
-              testing
-            </Link>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+              <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
           </div>
         </nav>
       </div>
