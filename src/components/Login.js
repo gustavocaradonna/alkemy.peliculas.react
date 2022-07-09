9921cf718d75705536c75343853842982958068e
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-07-09 01:19:39 -0300

    okk

--- src/components/Login.js
+++ src/components/Login.js
@@ -53,11 +53,11 @@ export function Login() {
     const bloque = document.querySelectorAll(".bounceGus");
     timeLine.to(bloque, {
       opacity: 1,
-      duration: 1,
+      duration: 2,
       // x: 200,
       // ease: "bounce",
       scale: 1,
-      stagger: 0.15,
+      stagger: 0.25,
     });
   }, []);
   return (
@@ -69,52 +69,52 @@ export function Login() {
           <div className="container py-5 h-100  ">
             <div className="row d-flex justify-content-center align-items-center h-100   ">
               <div className="col-12 col-md-8 col-lg-6 col-xl-5  ">
-                <div className="card bg-dark text-white ">
-                  <div className="card-body p- text-center greenGus">
-                    <div className="mb-md-5 mt-md-4 pb-5 ">
-                      <h2 className="fw-bold mb-2 bounceGus">Iniciar sesión</h2>
-                      <br />
+                <div className="card-body p- text-center greenGus">
+                  <div className="mb-md-5 mt-md-4 pb-5 ">
+                    <h2 className="fw-bold mb-2 text-white bounceGus">
+                      Iniciar sesión
+                    </h2>
+                    <br />
 
-                      <form onSubmit={submitHandler}>
-                        <div className="form-outline form-white mb-4 bounceGus">
-                          <input
-                            type="email"
-                            id="typeEmailX"
-                            className="form-control form-control-lg"
-                            name="email"
-                            placeholder="Ingrese email"
-                          />
-                        </div>
-
-                        <div className="form-outline form-white mb-4 bounceGus">
-                          <input
-                            type="password"
-                            id="typePasswordX"
-                            className="form-control form-control-lg"
-                            name="password"
-                            placeholder="Ingrese clave"
-                          />
-                        </div>
-                        <button
-                          className="btn btn-outline-light btn-lg px-5 bounceGus"
-                          type="submit"
-                        >
-                          Ingresar
-                        </button>
-                      </form>
-
-                      <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                        <a href="#!" className="text-white">
-                          <i className="fab fa-facebook-f fa-lg"></i>
-                        </a>
-                        <a href="#!" className="text-white">
-                          <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
-                        </a>
-                        <a href="#!" className="text-white">
-                          <i className="fab fa-google fa-lg"></i>
-                        </a>
+                    <form onSubmit={submitHandler}>
+                      <div className="form-outline form-white mb-4 bounceGus">
+                        <input
+                          type="email"
+                          id="typeEmailX"
+                          className="form-control form-control-lg"
+                          name="email"
+                          placeholder="Ingrese email"
+                        />
                       </div>
-                    </div>
+
+                      <div className="form-outline form-white mb-4 bounceGus">
+                        <input
+                          type="password"
+                          id="typePasswordX"
+                          className="form-control form-control-lg"
+                          name="password"
+                          placeholder="Ingrese clave"
+                        />
+                      </div>
+                      <button
+                        className="btn btn-outline-light btn-lg px-5 bounceGus"
+                        type="submit"
+                      >
+                        Ingresar
+                      </button>
+                    </form>
+
+                    {/* <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-facebook-f fa-lg"></i>
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-google fa-lg"></i>
+                      </a>
+                    </div> */}
                   </div>
                 </div>
               </div>
--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -24,7 +24,7 @@ export function Resultado() {
       .catch((error) => {
         alert(<h2>Hubo errores, intenta mas tarde!</h2>);
       });
-  }, []);
+  });
 
   return (
     <>
